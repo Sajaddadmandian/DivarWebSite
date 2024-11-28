ecabe7aa543568f3d45d1f43a933757e21414a00
sajaddadmandian <139868801+Sajaddadmandian@users.noreply.github.com>
2024-11-28 23:24:15 +0330

    fill auth controller and auth service and use sigleton design pattern

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,24 @@
+class AuthController {
+    constructor(){}
+    async sendOTP(req,res,next){
+        try {
+            
+        } catch (error) {
+            next (error)
+        }
+    }
+ async chechkOTP(req,res,next){
+        try {
+            
+        } catch (error) {
+            next (error)
+        }
+    }
+    async logout(req,res,next){
+        try {
+            
+        } catch (error) {
+            next (error)
+        }
+    }
+}
---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,10 @@
+class AuthService {
+    async sendOTP(mobile){
+        
+    }
+ async chechkOTP(movile, code){
+        
+    }
+    
+}
+module.exports = new AuthService()
